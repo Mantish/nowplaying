0996e255f0f2fd94507f2f7036c0e28fb59ec402
mantish <mauricio@8manos.com>
2015-12-03 15:14:15 -0500

    add twit to use the twitter api

--- nowplaying.js
+++ nowplaying.js
@@ -1,9 +1,23 @@
 if (Meteor.isClient) {
-  // codo to run only on the client
+  // code to run only on the client
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    Twit = new TwitMaker({
+      consumer_key: Meteor.settings.twitterKey,
+      consumer_secret: Meteor.settings.twitterSecret,
+      access_token: Meteor.settings.twitterToken,
+      access_token_secret: Meteor.settings.twitterTokenSecret
+    });
+
+    Twit.get(
+      'search/tweets',
+      {q: 'youtu #nowplaying filter:links', count: 5},
+      function(err, data, response) {
+        console.log(data)
+      }
+    );
   });
 }
