8f78990991d0656724d8b7e0903af90c61247b25
mantish <mauricio@8manos.com>
2015-12-07 16:35:08 -0500

    load youtube video before tweet

--- nowplaying.js
+++ nowplaying.js
@@ -1,18 +1,51 @@
 Tweets = new Mongo.Collection('tweets');
 
+onYouTubeIframeAPIReady = function() {
+  var video_divs = document.getElementsByClassName('youtube-video');
+
+  for (var i = 0; i < video_divs.length; i++) {
+    new YT.Player(video_divs[i], {
+      videoId: video_divs[i].dataset.videoId
+    });
+  }
+}
+
+getYoutubeId = function(tweet_urls) {
+  var reg_exp = /.*(youtu\.be\/|youtube(-nocookie)?\.com\/(vi?\/|.*u\/\w\/|embed\/|.*vi?=))([\w-]{11}).*/;
+
+  for (var i = 0; i < tweet_urls.length; i++) {
+    tweet_urls[i]
+    var match = tweet_urls[i].expanded_url.match(reg_exp);
+    if (match) {
+      return match[4];
+    }
+  };
+}
+
 if (Meteor.isClient) {
   Meteor.subscribe('recent-tweets');
 
   Template.body.helpers({
     tweets: function () {
-      return Tweets.find();
+      var tweets = Tweets.find().fetch();
+      tweets.forEach(function(el, index) {
+        tweets[ index ].youtube_id = getYoutubeId(el.entities.urls);
+      });
+
+      return tweets;
     }
   });
 
   Template.tweet.onRendered(function () {
     //if twitter js is loaded before the templates are rendered, then we have to call it manually
-    if (twttr) {
+    if (typeof twttr.widgets != 'undefined') {
       twttr.widgets.load(this.firstNode);
+    }
+
+    if (typeof YT != 'undefined' && YT.loaded) {
+      new YT.Player(this.firstNode.children[0], {
+        videoId: this.data.youtube_id
+      });
     }
   });
 }
